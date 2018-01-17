4985a3336f92e009aba31c520e751ea18b4e18cc
Janosch Müller <janosch.mueller@betterplace.org>
2018-01-17 13:07:11 +0100

    Run format helper test with fixed locale

--- test/helpers/format_amount_test.jsx
+++ test/helpers/format_amount_test.jsx
@@ -2,6 +2,7 @@ import { formatAmount } from '../../src'
 
 describe('formatAmount', () => {
   it('formats cent amounts with rounding', () => {
+    I18n.locale = 'de'
     expect(formatAmount({cents: 99999})).toMatch(/1\.000\D/)
   })
 })
