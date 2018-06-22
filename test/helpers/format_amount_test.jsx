6ecd803e16dd22d8d30b15fa7ebb0e89dbda7fe8
Jannosch Müller <janosch.mueller@betterplace.org>
2018-06-22 14:57:46 +0200

    Shut this brittle test up

--- test/helpers/format_amount_test.jsx
+++ test/helpers/format_amount_test.jsx
@@ -2,6 +2,6 @@ import { formatAmount } from '../../src'
 
 describe('formatAmount', () => {
   it('formats cent amounts with rounding', () => {
-    expect(formatAmount({cents: 99999})).toMatch(/(1\.000\s€|€\s1,000)/)
+    expect(formatAmount({cents: 99999})).toMatch(/(1[.,]000\s€|€\s1[.,]000)/)
   })
 })
