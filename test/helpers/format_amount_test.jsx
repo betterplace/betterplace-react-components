751ab36ae38d78cf3ea582cc822076309ab64561
Jannosch Müller <janosch.mueller@betterplace.org>
2018-09-21 12:03:50 +0200

    Add showCents option to formatAmount

--- test/helpers/format_amount_test.jsx
+++ test/helpers/format_amount_test.jsx
@@ -4,4 +4,9 @@ describe('formatAmount', () => {
   it('formats cent amounts with rounding', () => {
     expect(formatAmount({cents: 99999})).toMatch(/(1[.,]000\s?€|€\s?1[.,]000)/)
   })
+
+  it('includes 2 fraction digits for showCents: true', () => {
+    expect(formatAmount({cents: 99999, showCents: true})).toMatch(/999[.,]99/)
+    expect(formatAmount({cents: 100, showCents: true})).toMatch(/1[.,]00/)
+  })
 })
