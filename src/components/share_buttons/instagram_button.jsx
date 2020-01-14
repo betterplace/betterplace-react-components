2b203d035a53c7dee5691e2e410b27042d2b2351
Katrin Fritz <katrinfritz@web.de>
2020-01-14 10:18:22 +0100

    Remove utm params for instagram button

--- src/components/share_buttons/instagram_button.jsx
+++ src/components/share_buttons/instagram_button.jsx
@@ -2,8 +2,8 @@ import { InstagramIcon } from '../../assets/icons'
 import { buildShareButtonComponent } from './base'
 import { toQuery, copyToClipboard } from '../../helpers/sharing_helper'
 
-export const instagramAction = ({ shareUrl, utmParams }) => {
-  copyToClipboard(`${shareUrl}${toQuery(utmParams || {})}`)
+export const instagramAction = ({ shareUrl }) => {
+  copyToClipboard(shareUrl)
 }
 
 export const InstagramButton = buildShareButtonComponent({
