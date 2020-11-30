97bf3c6e3caf7f2f68fa0e050cf38d36017adad4
Katrin Fritz <katrinfritz@web.de>
2020-11-30 10:57:11 +0100

    Add donations amount to fundraising event teaser [#175910677]

--- src/components/fundraising_event_teaser.jsx
+++ src/components/fundraising_event_teaser.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { formatAmount } from '../helpers/format_amount'
 
 export const FundraisingEventTeaser = (props) => {
   const fundraisingEvent = props.fundraisingEvent
   const locale = props.locale || document.documentElement.lang || 'de'
   const href = props.href || fundraisingEvent.links.find(link => link.rel === 'platform')?.href
   const fundraisingEventImageUrl = fundraisingEvent.profile_picture.links.find(link => link.rel === 'fill_410x214')?.href
+  const donationsAmount = formatAmount({ cents: fundraisingEvent.donated_amount_in_cents, locale: locale })
   const contactImageUrl = fundraisingEvent.contact.picture.links.find(link => link.rel === 'fill_100x100')?.href
   const handleClick = (event) => { props.onClick && props.onClick(event, fundraisingEvent) }
 
@@ -25,8 +27,8 @@ export const FundraisingEventTeaser = (props) => {
     <div className='donatable-teaser--divider'>
       <div className='donatable-teaser--factlist'>
         <div><span dangerouslySetInnerHTML={{ __html: fundraisingEvent.contact.name }}/></div>
-        {fundraisingEvent.category && <div className='donatable-teaser--badge'>{fundraisingEvent.category}</div>}
         <div><span>{fundraisingEvent.donations_count}</span> {fundraisingEvent.donations_count === 1 ? (locale === 'de' ? 'Spende' : 'donation') : (locale === 'de' ? 'Spenden' : 'donations')}</div>
+        <div><span>{donationsAmount}</span> {locale === 'de' ? 'gesammelt' : 'collected'}</div>
       </div>
       <img style={{ borderRadius: '50%', border: '1px solid #cccccc'}} src={contactImageUrl} alt={fundraisingEvent.contact.name} className='donatable-teaser--carrier-logo' />
     </div>
